37a141e3f5f765960407bab6f6d8ff2edf1736f1
barbxsa <aopgabriel@gmail.com>
2025-08-23 00:59:24 -0300

    Ajustes no Mobile e Dark Mode

--- src/routes/logsRoutes.ts
+++ src/routes/logsRoutes.ts
@@ -5,21 +5,21 @@ import { open } from 'sqlite';
 const router = Router();
 
 async function openDb() {
-  return open({
-    filename: './banco.db',
-    driver: sqlite3.Database
-  });
+    return open({
+        filename: './banco.db',
+        driver: sqlite3.Database
+    });
 }
 
 router.get('/', async (req, res) => {
-  try {
-    const db = await openDb();
-    const logs = await db.all('SELECT * FROM log ORDER BY id DESC');
-    res.json(logs);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ erro: 'Erro ao buscar logs' });
-  }
+    try {
+        const db = await openDb();
+        const logs = await db.all('SELECT * FROM log ORDER BY id DESC');
+        res.json(logs);
+    }catch (error) {
+        console.error(error);
+        res.status(500).json({ erro: 'Erro ao buscar logs' });
+    }
 });
 
 export default router;
